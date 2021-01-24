320813085dce18525d09c8e84a226411223f3663
Nils Wisiol <mail@nils-wisiol.de>
2021-01-24 15:58:44 +0100

    increase compatibility with old versions of docker-compose -__-

--- addzone.sh
+++ addzone.sh
@@ -8,20 +8,17 @@ if [ -z "$ZONE" ]; then
 fi
 
 echo "Setting up a minimalistic zone for $ZONE"
-
-cat << EOF | docker-compose exec -T ns knotc
-conf-begin
-conf-set zone[$ZONE]
-conf-set zone[$ZONE].dnssec-policy default
-conf-set zone[$ZONE].dnssec-signing on
-conf-commit
-zone-begin $ZONE
-zone-set $ZONE @ 7200 SOA get.desec.io. get.desec.io. 2021014779 86400 86400 2419200 3600
-zone-set $ZONE @ 3600 A 127.0.0.1
-zone-set $ZONE @ 3600 AAAA ::1
-zone-set $ZONE @ 3600 TXT "agility"
-zone-commit $ZONE
-EOF
+docker-compose exec ns knotc conf-begin
+docker-compose exec ns knotc conf-set zone[$ZONE]
+docker-compose exec ns knotc conf-set zone[$ZONE].dnssec-policy default
+docker-compose exec ns knotc conf-set zone[$ZONE].dnssec-signing on
+docker-compose exec ns knotc conf-commit
+docker-compose exec ns knotc zone-begin $ZONE
+docker-compose exec ns knotc zone-set $ZONE @ 7200 SOA get.desec.io. get.desec.io. 2021014779 86400 86400 2419200 3600
+docker-compose exec ns knotc zone-set $ZONE @ 3600 A 127.0.0.1
+docker-compose exec ns knotc zone-set $ZONE @ 3600 AAAA ::1
+docker-compose exec ns knotc zone-set $ZONE @ 3600 TXT "agility"
+docker-compose exec ns knotc zone-commit $ZONE
 
 echo "Adding keys"
 docker-compose exec ns keymgr $ZONE. generate zsk=true ksk=true algorithm=rsasha1 size=1024
